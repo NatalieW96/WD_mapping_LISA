9277a2d4d3eb352fd0d11b2d1f382e5c84d115d0
Natalie <natty@katendpete.net>
2018-11-19 17:37:11 +0000

    made faster

--- LISA_plot.py
+++ LISA_plot.py
@@ -18,20 +18,23 @@ def l(phi_b, t, T, alpha0, i):
     x_b= -np.cos(alpha_i)*np.sin(phi_b) + 0.5*np.sin(alpha_i)*np.cos(phi_b)
     y_b= np.cos(alpha_i)*np.cos(phi_b) + 0.5*np.sin(alpha_i)*np.sin(phi_b)
     z_b= (np.sqrt(3)/2.0)*np.sin(alpha_i)
-    return [x_b, y_b, z_b]
+    return np.transpose(np.array([x_b, y_b, z_b]))
 
 #Equation to give phi_b
 def phi_bar(phi0, T, t):
     return phi0 +2*np.pi*t/T
 
 #Function to give response
-def response(A0, Omega, t, Phi0, psi, iota, l1, l2):
+def response(A0, Omega, t, Phi0, psi, iota, l1, l2, h_ab):
     h_plus = A0*np.sin(Omega*t - Phi0) #plus response from WD
     h_x = A0*np.cos(Omega*t - Phi0)	#cross response from WD
-    h_ab= np.array([[h_plus, h_x, 0],[h_x, -h_plus, 0], [0, 0, 0]]) #response tensor
+    h_ab[:,0,0]= h_plus
+    h_ab[:,0,1]= h_x
+    h_ab[:,1,0]= h_x
+    h_ab[:,1,1]= -h_plus
     R=np.array([[psi,iota, 0.0], [-iota, psi, 0.0], [0,0.0,1.0]])
     h_ab= np.transpose(R)*h_ab*R
-    h_i = 0.5*h_ab*(np.outer(l1,l1)- np.outer(l2,l2)) #response at LISA for WD q at time j
+    h_i = 0.5*h_ab*(np.einsum('ki,kj->kij', l1, l1)- np.einsum('ki,kj->kij', l2, l2)) #response at LISA for WD q at time j
     return h_i
 
 #Load WD positions and parameter files
@@ -41,7 +44,7 @@ with open('WD_parameters.sav') as data:
     WD_params=pickle.load(data)
 
 T=3.15e7		#seconds in a year
-N=1000			#number of time intervals
+N=100000		#number of time intervals
 dt=T/N			#time interval
 df=1.0/T		#frequency interval
 phi0=0			#starting phi value	
@@ -53,6 +56,7 @@ phi_b=np.zeros((N,3))	#phi_b array
 l_i=np.zeros((N,3,3))	#l_i array
 V=np.zeros((N,3,3))	#vertices coordinates
 h_I=np.zeros((N,3,3))   #total response
+h_ab2=np.zeros((N,3,3))
 h_mod=np.zeros((N))	#modulus of response
 n=3			#number of arms
 theta_b=np.pi/2		
@@ -67,30 +71,31 @@ Omega=WD_params[3]
 Phi0=WD_params[4]
 N_WD=len(WD_pos[0,0]) 	#Number of WDs
 h_ab=np.zeros((N_WD,N,3,3))#signal tensor for WDs
-h_i=np.zeros((3,3))	#Individual response of WD binary on LISA
 
 
 #Fill in l_i vector at each point in time for each arm
-for j,t in enumerate(time):
-    for i in np.arange(n):	
-	phi_b[j,i]= phi_bar(phi0, T, t) 
-	l_i[j,i] = l(phi_b[j,i], t, T, alpha0, i+1)
-    r_12=-(l_i[j,0]+l_i[j,1])#trying to get vector to vertex
-    r_12=r*r_12/np.linalg.norm(r_12)
-    C=[R*np.cos(omega*t),R*np.sin(omega*t),z_c]	#coordinates of centre of circle
-    v_12=C+r_12 	#vertex at arm 1 and 2
-    v_23=v_12+L*l_i[j,0]	#vertes at arm 2 and 3
-    v_31=v_12+L*l_i[j,1]  #vertex at arm 3 and 1
-    V[j,:]=[v_12, v_23, v_31]	#array of vertices
-    
-    #Go through each WD binary and calulate response at any given time
-    for q in np.arange(N_WD):
-	h_i=response(A0[q,0], Omega[q,0], t, Phi0[q,0], psi[q,0], iota[q,0], l_i[j,0], l_i[j,1])
-	h_I[j] = h_I[j] + h_i #sum all WD responses at time j
-    h_mod[j] = np.linalg.norm(h_I[j]) #modulus od response tensor at time 
+for i in np.arange(n):	
+    phi_b[:,i]= phi_bar(phi0, T, time) 
+    l_i[:,i] = l(phi_b[:,i], time, T, alpha0, i+1)
+r_12=-(l_i[0]+l_i[1])#trying to get vector to vertex
+r_12=r*r_12/np.linalg.norm(r_12)
+C=[R*np.cos(omega*time),R*np.sin(omega*time),z_c]	#coordinates of centre of circle
+v_12=C+r_12 	#vertex at arm 1 and 2
+v_23=v_12+L*l_i[0]	#vertes at arm 2 and 3
+v_31=v_12+L*l_i[1]  #vertex at arm 3 and 1
+V=[v_12, v_23, v_31]	#array of vertices
 
-h_modf=np.fft.rfft(h_mod)*dt
-freq=np.arange(0, df*len(h_modf), df)
+  
+#Go through each WD binary and calulate response at any given time
+for q in np.arange(N_WD):
+    h_i=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,0], l_i[:,1], h_ab2)
+    h_I = h_I + h_i #sum all WD responses at time j
+
+#for j in np.arange(N):
+#    h_mod[j]=np.linalg.norm(h_I[j])
+
+h_f=np.fft.rfft(h_I)*dt
+freq=np.arange(0, df*len(h_I), df)
 fig = plt.figure()
 plt.plot(time, h_mod)
 plt.xlabel('t (s)')
