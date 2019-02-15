3c4ef84870cdf3c19d49b14cae4ca9261cf42b3a
Natalie <natty@katendpete.net>
2019-02-15 18:32:19 +0000

    seperated expectation and data generation and rewrote code

--- LISA_plot.py
+++ LISA_plot.py
@@ -12,6 +12,7 @@ import mpl_toolkits.mplot3d.axes3d as p3
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from mpl_toolkits.mplot3d import Axes3D
 import time as tm
+import os.path
 
 #Equation to give vector of a LISA arm
 def l(phi_b, t, T, alpha0, i):
@@ -44,8 +45,8 @@ def response(A0, Omega, t, Phi0, psi, iota, l1, l2, h_ab, alpha, beta):
     h_t = np.einsum('kij,kij->k',D_ij,h_ab3)
     return h_t
 
-#Function to give shape
-def expectation(A0, psi, iota, l1, l2, h_ab, alpha, beta):
+#Function to expectation response
+def expectation_response(A0, psi, iota, l1, l2, h_ab, alpha, beta):
     h_plus = 0.5*(1+np.cos(iota)**2)*A0*1/np.sqrt(2) #plus response from WD
     h_x = np.cos(iota)*A0*1/np.sqrt(2)	#cross response from WD
     h_ab[:,0,0] = h_plus
@@ -68,93 +69,135 @@ def SNR(h,f):
     alpha=10**(-22.79)*(f/10**-3)**(-7/3)
     beta=10**(-24.54)*(f/10**-3)
     gamma=10**(-23.04)
-    noise=5.049e5*(alpha**2+beta**2+gamma**2)
+    noise=np.sqrt(5.049e5*(alpha**2+beta**2+gamma**2))
     return h/noise
 
-#Load WD positions and parameter files
-with open('WD_positions_1000000_const_iota_psi.sav') as data:
-    WD_pos=pickle.load(data)
-with open('WD_parameters_1000000_const_iota_psi.sav') as data:
-    WD_params=pickle.load(data)
+#Function to give LISA arm vectors over period T
+def arm_vectors(n,N,T,phi0, alpha0,time):
+    phi_b=np.zeros((N,3))
+    l_i=np.zeros((N,3,3))	#l_i
+    for i in np.arange(n):	
+        phi_b[:,i]= phi_bar(phi0, T, time) 
+        l_i[:,i] = l(phi_b[:,i], time, T, alpha0, i+1)
+    return l_i
+
+#Function to create points to be plotted for LISA arms
+def plot_arms(l_i,R,L,omega,time,z_c):
+    r=(L/2.0)/np.cos(np.pi/6.0) #length of vector to vertex
+    r_12=-(l_i[0]+l_i[1])#trying to get vector to vertex
+    r_12=r*r_12/np.linalg.norm(r_12)
+    C=[R*np.cos(omega*time),R*np.sin(omega*time),z_c]	#coordinates of centre of circle
+    v_12=C+r_12 	#vertex at arm 1 and 2
+    v_23=v_12+L*l_i[0]	#vertes at arm 2 and 3
+    v_31=v_12+L*l_i[1]  #vertex at arm 3 and 1
+    V=[v_12, v_23, v_31]	#array of vertices
+
+#Function to define parameters used
+def parameter_extraction(params, pos):
+    psi=params[0] 
+    iota=params[1]
+    A0=params[2]
+    Omega=params[3]
+    Phi0=params[4]
+    r_WD=pos[3,0]
+    N_WD=len(pos[0,0])
+    #Euler angles for ecliptic transformation of WDs
+    Z_WD=pos[2]
+    Z_WD=-Z_WD/np.sqrt(Z_WD[0]**2 + Z_WD[1]**2 + Z_WD[2]**2) #normalised WD Z axis vector in ecliptic frame
+    alpha = np.arccos(-Z_WD[1]/np.sqrt(1-Z_WD[2]**2)) 
+    beta = np.arccos(Z_WD[2])
+    return psi, iota, A0, Omega, Phi0, r_WD, N_WD, alpha, beta
+
+#Function to give the expectation values
+def expectation(N_WD, A0, psi, iota, l_i, h_ab2, alpha, beta, h_a_h_b_exp):
+    fig=plt.figure()
+    for q in np.arange(N_WD):
+        h_i1exp=expectation_response(A0[q,0], psi[q,0], iota[q,0], l_i[:,0], l_i[:,1], h_ab2, alpha[q,0], beta[q,0])
+        h_i2exp=expectation_response(A0[q,0], psi[q,0], iota[q,0], l_i[:,1], l_i[:,2], h_ab2, alpha[q,0], beta[q,0])
+        h_i3exp=expectation_response(A0[q,0], psi[q,0], iota[q,0], l_i[:,2], l_i[:,0], h_ab2, alpha[q,0], beta[q,0])
+        h_a_h_b_exp= h_a_h_b_exp + np.array([[h_i1exp*h_i1exp,h_i1exp*h_i2exp,h_i1exp*h_i3exp],[h_i2exp*h_i1exp,h_i2exp*h_i2exp,h_i2exp*h_i3exp], [h_i3exp*h_i1exp,h_i3exp*h_i2exp,h_i3exp*h_i3exp]])
+        if q > 1 and np.log10(q+1)%1 == 0:
+            print q+1
+            plt.plot(time, h_a_h_b_exp[0,0]/(q+1), alpha=1, label= q+1) 
+            plt.show()
+            break
+    return h_a_h_b_exp
+
+#Function to give the simulated data
+def signal(N_WD,A0,Omega,time,Phi0,psi,iota,l_i,h_ab2, alpha, beta, h_I1, h_I2, h_I3):
+    for q in np.arange(N_WD):
+        h_i1=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,0], l_i[:,1], h_ab2, alpha[q,0], beta[q,0])
+        h_I1 = h_I1 + h_i1 
+        h_i2=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,1], l_i[:,2], h_ab2, alpha[q,0], beta[q,0])
+        h_I2 = h_I2 + h_i2 
+        h_i3=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,2], l_i[:,0], h_ab2, alpha[q,0], beta[q,0])
+        h_I3 = h_I3 + h_i3
+        break
+    return np.array([h_I1, h_I2, h_I3])
 
 T=3.15e7		#seconds in a year
-N=100			#number of time intervals
-dt=T/N			#time interval
-df=1.0/T		#frequency interval
-phi0=0			#starting phi value	
-alpha0=0		#starting alpha value
+n=3			#arms of LISA
+phi0=0			#starting phi value
+alpha0=0		#starting alpha value	
 R=1.496e11		#earth sun distance
 L=5.0e10		#length of LISA arms
-r=(L/2.0)/np.cos(np.pi/6.0) #length of vector to vertex
-phi_b=np.zeros((N,3))	#phi_b array
-l_i=np.zeros((N,3,3))	#l_i array
-V=np.zeros((N,3,3))	#vertices coordinates
+z_c=0.0			#LISA orbit plane in x, y
+omega=2*np.pi/T		#angular speed of orbit
+
+#Load WD positions and parameter files
+with open('WD_positions_1000000_const_iota_psi_data.sav') as data:
+    WD_pos_data=pickle.load(data)
+with open('WD_parameters_1000000_const_iota_psi_data.sav') as data:
+    WD_params_data=pickle.load(data)
+with open('Gal_parameters_1000000_const_iota_psi_data.sav') as data:
+    Gal_params_data=pickle.load(data)
+
+#Check if expectation values have already been generated, and load if so
+if os.path.isfile('./expectation_values_{}_{}_{}_const_iota_psi.sav'.format(Gal_params_data[0],Gal_params_data[1],Gal_params_data[2])):
+    with open('expectation_values_{}_{}_{}_const_iota_psi.sav'.format(Gal_params_data[0],Gal_params_data[1],Gal_params_data[2])) as data:
+        exp_values=pickle.load(data)
+    print "expectation values recovered"
+else:
+    print "expectation values must be created"
+    with open('WD_positions_1000000_const_iota_psi_exp.sav') as data:
+        WD_pos_exp=pickle.load(data)
+    with open('WD_parameters_1000000_const_iota_psi_exp.sav') as data:
+        WD_params_exp=pickle.load(data)
+    with open('Gal_parameters_1000000_const_iota_psi_exp.sav') as data:
+        Gal_params_exp=pickle.load(data)
+    if (Gal_params_data !=Gal_params_exp).all():		#Check that galaxy parameters are consistant between data and expectation values
+        print "Error: Galaxy parameters must be the same" 
+        exit()
+    print "data loaded"
+    N=100			#number of time intervals
+    dt=T/N			
+    time = np.arange(0,T,dt)
+    l_i= arm_vectors(n,N,T,phi0, alpha0, time)	#create arm vectors for N time segments
+    psi, iota, A0, Omega, Phi0, r_WD, N_WD, alpha, beta = parameter_extraction(WD_params_exp,WD_pos_exp)	#recover parameters from simulation files
+    h_a_h_b_exp=np.zeros((3,3,N))
+    h_ab2=np.zeros((N,3,3))
+    exp_values = expectation(N_WD, A0, psi, iota, l_i, h_ab2, alpha, beta, h_a_h_b_exp)		#create expectation values
+    pickle.dump(exp_values, open('expectation_values_{}_{}_{}_const_iota_psi.sav'.format(Gal_params_exp[0],Gal_params_exp[1],Gal_params_exp[2]), 'wb'))
+    print "Expectation values saved"
+#Data generation
+N=100				#number of time intervals
+dt=T/N
+time = np.arange(0,T,dt)
+l_i= arm_vectors(n,N,T,phi0, alpha0, time)	#create arm vectors for N time segments
+psi, iota, A0, Omega, Phi0, r_WD, N_WD, alpha, beta = parameter_extraction(WD_params_data,WD_pos_data)		#recover parameters from simulation files
 h_I1=np.zeros(N)   #total response interferomenter 1
 h_I2=np.zeros(N)   #total response interferomenter 2
 h_I3=np.zeros(N)   #total response interferomenter 3
-h_ab2=np.zeros((N,3,3))
-h_mod=np.zeros((N))	#modulus of response
-n=3			#number of arms
-theta_b=np.pi/2		
-omega=2*np.pi/T		#angular speed of orbit
-time = np.arange(0,T,dt)#time array
-z_c=0.0			#LISA orbit plane in x, y
-
-#WD parameters
-psi=WD_params[0] 
-iota=WD_params[1]
-A0=WD_params[2]
-Omega=WD_params[3]
-Phi0=WD_params[4]
-r_WD=WD_pos[3,0]
-N_WD=len(WD_pos[0,0]) 	#Number of WDs
+h_ab2=np.zeros((N,3,3))	
 h_ab=np.zeros((N_WD,N,3,3))#signal tensor for WDs
-#Euler angles for ecliptic transformation of WDs
-Z_WD=WD_pos[2]
-Z_WD=-Z_WD/np.sqrt(Z_WD[0]**2 + Z_WD[1]**2 + Z_WD[2]**2) #normalised WD Z axis vector in ecliptic frame
-alpha = np.arccos(-Z_WD[1]/np.sqrt(1-Z_WD[2]**2)) 
-beta = np.arccos(Z_WD[2])
-h_a_h_b_exp=np.zeros((3,3,N))
-signal=np.zeros((N_WD))
+data= signal(N_WD,A0,Omega,time,Phi0,psi,iota,l_i,h_ab2, alpha, beta, h_I1, h_I2, h_I3)		#create signal for each LISA arm
+exit()
 
-#Fill in l_i vector at each point in time for each arm
-for i in np.arange(n):	
-    phi_b[:,i]= phi_bar(phi0, T, time) 
-    l_i[:,i] = l(phi_b[:,i], time, T, alpha0, i+1)
-r_12=-(l_i[0]+l_i[1])#trying to get vector to vertex
-r_12=r*r_12/np.linalg.norm(r_12)
-C=[R*np.cos(omega*time),R*np.sin(omega*time),z_c]	#coordinates of centre of circle
-v_12=C+r_12 	#vertex at arm 1 and 2
-v_23=v_12+L*l_i[0]	#vertes at arm 2 and 3
-v_31=v_12+L*l_i[1]  #vertex at arm 3 and 1
-V=[v_12, v_23, v_31]	#array of vertices
 
-#Go through each WD binary and calulate response at any given time
-fig=plt.figure()
-for q in np.arange(N_WD):
-    h_i1=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,0], l_i[:,1], h_ab2, alpha[q,0], beta[q,0])
-    h_I1 = h_I1 + h_i1 #sum all WD responses at time j
-    h_i2=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,1], l_i[:,2], h_ab2, alpha[q,0], beta[q,0])
-    h_I2 = h_I2 + h_i2 #sum all WD responses at time j
-    h_i3=response(A0[q,0], Omega[q,0], time, Phi0[q,0], psi[q,0], iota[q,0], l_i[:,2], l_i[:,0], h_ab2, alpha[q,0], beta[q,0])
-    h_I3 = h_I3 + h_i3 #sum all WD responses at time j
-    h_i1exp=expectation(A0[q,0], psi[q,0], iota[q,0], l_i[:,0], l_i[:,1], h_ab2, alpha[q,0], beta[q,0])
-    h_i2exp=expectation(A0[q,0], psi[q,0], iota[q,0], l_i[:,1], l_i[:,2], h_ab2, alpha[q,0], beta[q,0])
-    h_i3exp=expectation(A0[q,0], psi[q,0], iota[q,0], l_i[:,2], l_i[:,0], h_ab2, alpha[q,0], beta[q,0])
-    signal[q] = SNR(max(h_i1exp), Omega[q,0])
-    h_a_h_b_exp= h_a_h_b_exp + np.array([[h_i1exp*h_i1exp,h_i1exp*h_i2exp,h_i1exp*h_i3exp],[h_i2exp*h_i1exp,h_i2exp*h_i2exp,h_i2exp*h_i3exp], [h_i3exp*h_i1exp,h_i3exp*h_i2exp,h_i3exp*h_i3exp]])
-#    print '{}: done {}/{}'.format(tm.asctime(),q+1,N_WD)
-    if q > 1 and np.log10(q+1)%1 == 0:
-        print q+1
-        plt.plot(time, h_a_h_b_exp[0,0]/(q+1), alpha=1, label= q+1)
-#expectation value calculations
-plt.xlabel('Time (s)')
+#PLOTTING
 plt.ylabel('<$h^{I}(t)><h^{I}(t)$>')
 plt.legend()
 plt.show()
-
-pickle.dump(h_a_h_b_exp, open('expectation_values_const_iota_psi.sav', 'wb'))
-pickle.dump(signal, open('SNR_{}WDs_const_iota_psi.sav'.format(N_WD), 'wb'))
 
 plt.hist(A0, bins='auto')
 plt.show()
--- galaxy_model.py
+++ galaxy_model.py
@@ -22,9 +22,16 @@ def data_for_cylinder_along_z(center_x,center_y,radius,height_z):
     y_grid = radius*np.sin(theta_grid) + center_y
     return x_grid,y_grid,z_grid
 
+#function of SNR distanct checking
+def SNR_check(theta,rho,z,thetaS,rhoS,zS,r_SNR):
+    if np.sqrt((rho*np.cos(theta)-rhoS*np.cos(thetaS))**2 +(rho*np.sin(theta)-rhoS*np.cos(thetaS))**2 +(z-zS)**2)>r_SNR:
+        return 1
+    else:
+        return 0
+
 #Define Milky Way parameters
 h0=1000*9.461e15			#height of MW
-r0=100000*9.461e15 		#radius of MW
+r0=6e20 		#radius of MW
 N=1000000	   		#number of binary WDs
 
 #Sun's parameters
@@ -32,6 +39,7 @@ rhoS=25000*9.461e15
 thetaS=0.0
 zS=500*9.461e15
 beta= np.pi*60.2/180 #angle of ecliptic
+r_SNR=2.523e18 #minimum distance for SNR boundry
 
 #Initialise coordinates
 theta=np.zeros((N,1))
@@ -45,17 +53,23 @@ iota=np.zeros((N,1))
 A0=np.zeros((N,1))
 phi0=np.zeros((N,1))
 omega=np.zeros((N,1))
-
+f=1e-4 #for const freq
 
 #Generate random coordinates
 for i in np.arange(N):
-    theta[i]=random.uniform(0, 2*np.pi)
-    rho[i]=r0*np.sqrt(random.uniform(0,1.0))
-    z[i]=random.uniform(0,h0)
-    psi[i]=np.pi/2
-    iota[i]=np.pi/2
-    phi0[i]=random.uniform(0, 2*np.pi)
-    omega[i]=random.uniform(10e-3, 10e-2)
+    theta1=random.uniform(0, 2*np.pi)
+    rho1=r0*np.sqrt(random.uniform(0,1.0))
+    z1=random.uniform(0,h0)
+    if SNR_check(theta1,rho1,z1,thetaS,rhoS,zS,r_SNR)==1:
+        theta[i]=theta1
+        rho[i]=rho1
+        z[i]=z1
+        psi[i]=np.pi/2
+        iota[i]=np.pi/2
+        phi0[i]=random.uniform(0, 2*np.pi)
+        omega[i]=f
+    else:
+        i=i-1
     print '{}: done {}/{}'.format(tm.asctime(),i+1,N)
 
 x=rho*np.cos(theta)	#x coordinates of WDs
@@ -70,7 +84,6 @@ Z=z-zS
 
 #Setting Galactic coordinates
 r = np.sqrt(X**2 +Y**2 +Z**2)
-print r
 theta_gal=np.arccos(Z/r)
 lon_gal=np.arctan2(Y,X)
 lat_gal=np.pi/2-theta_gal
@@ -86,11 +99,11 @@ Y_ec=r*np.sin(theta_ec)*np.sin(lon_ec)
 Z_ec=r*np.cos(theta_ec)
 
 #Amplitude calculation
-M_WD=(1.989e30)*0.5	#WD mass
+M_WD=1.989e30	#WD mass
 D_WD=10**8	#WD seperation distance
 G=6.67e-11
 c=3e8
-A0=(G*(M_WD**2))/(D_WD*(c**4))
+A0=32*np.pi**2*(G*M_WD*(D_WD**2)*(f**2))/(c**4)
 A= A0/r  #gravitational wave amplitude
 
 #Plot 3D graph
@@ -134,8 +147,10 @@ plt.show()
 
 print 'saving files'
 positions = np.array([[X, Y, Z],[r, lon_gal, lat_gal],[X_ec, Y_ec, Z_ec], [r, lon_ec, lat_ec]])
-pickle.dump(positions, open('WD_positions_{}_const_iota_psi.sav'.format(N), 'wb'))
-parameters = np.array([psi, iota, A, omega, phi0]) 
-pickle.dump(parameters, open('WD_parameters_{}_const_iota_psi.sav'.format(N), 'wb'))
+pickle.dump(positions, open('WD_positions_{}_const_iota_psi_exp.sav'.format(N), 'wb'))
+WD_parameters = np.array([psi, iota, A, omega, phi0]) 
+pickle.dump(WD_parameters, open('WD_parameters_{}_const_iota_psi_exp.sav'.format(N), 'wb'))
+Gal_parameters= np.array([h0,r0,beta]) 
+pickle.dump(Gal_parameters, open('Gal_parameters_{}_const_iota_psi_exp.sav'.format(N), 'wb'))
 print 'done'
 exit()
